fc4aaea46edbb46ec0ab273985e8eab1e7aa0cdb
Xenocynic <danktangent.mxc@gmail.com>
2025-08-09 12:55:15 -0600

    wiring buttons to resp. pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+
+import Index from "@/pages/Index";
+import ModulesPage from "@/pages/Modules";
+import ExercisesPage from "@/pages/Exercises";
+import ProgressPage from "@/pages/Progress";
+import NotFound from "@/pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +20,10 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/modules" element={<ModulesPage />} />
+          <Route path="/exercises" element={<ExercisesPage />} />
+          <Route path="/progress" element={<ProgressPage />} />
+          {/* keep this last */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
